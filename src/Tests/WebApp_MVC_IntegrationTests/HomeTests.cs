52dda16dbcdcc5f67db5dfca41ba13c2351664e5
usman_solemn <usman.solemn@gmail.com>
2021-04-03 19:03:17 +0500

    Added end to end testing project as well

---
+++ src/Tests/WebApp_MVC_IntegrationTests/HomeTests.cs
@@ -0,0 +1,37 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using WebApp_MVC;
+using Xunit;
+
+namespace WebApp_MVC_IntegrationTests
+{
+    public class HomeTests
+    {
+        public class WarehouseTests : IClassFixture<TestFixture<Startup>>
+        {
+            private HttpClient Client;
+
+            public WarehouseTests(TestFixture<Startup> fixture)
+            {
+                Client = fixture.Client;
+            }
+
+            [Fact]
+            public async Task TestGetHomeIndexAsync()
+            {
+                // Arrange
+                var request = "/";
+
+                // Act
+                var response = await Client.GetAsync(request);
+
+                // Assert
+                //response.EnsureSuccessStatusCode();
+                var responseString = await response.Content.ReadAsStringAsync();
+                Assert.True(responseString.Length > 0);
+            }
+
+
+        }
+    }
+}
