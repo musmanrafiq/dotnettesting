aa3a18af0415014af537fba4f88bf0ecec6e62e8
usman_solemn <usman.solemn@gmail.com>
2021-04-03 15:55:33 +0500

    First commet with first baseline for this project

---
+++ src/Library/OpenWeatheWrapper/Interfaces/IWheatherService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenWeatheWrapper.Interfaces
+{
+    public interface IWheatherService
+    {
+        Task GetWeatherByCity(string cityName);
+    }
+}
---
+++ src/Library/OpenWeatheWrapper/Services/WheatherService.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Options;
+using OpenWeatheWrapper.Interfaces;
+using OpenWeatheWrapper.IOptions;
+using OpenWeatheWrapper.Models;
+using System;
+using System.Net.Http;
+using System.Net.Http.Json;
+using System.Threading.Tasks;
+
+namespace OpenWeatheWrapper.Services
+{
+    public class WheatherService : IWheatherService
+    {
+        private readonly  HttpClient _client;
+        private readonly OpenWeatherOptions _weatherOptions;
+
+        public WheatherService(IHttpClientFactory clientFactory, IOptionsSnapshot<OpenWeatherOptions> options)
+        {
+            _client = clientFactory.CreateClient("WeatherApiClient");
+            _weatherOptions = options.Value;
+        }
+        public async Task GetWeatherByCity(string cityName)
+        {
+            HttpResponseMessage response = await _client.GetAsync($"/data/2.5/weather?q=islamabad&appid={_weatherOptions.AppId}");
+            if (response.IsSuccessStatusCode)
+            {
+                var ia = await response.Content.ReadFromJsonAsync<WeatherModel>();
+            }
+
+            // return URI of the created resource.
+            var a =  response.Headers.Location;
+        }
+    }
+}
